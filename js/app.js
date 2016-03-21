3fa89f83a00cb3736de07e13140ebe3e6e8e3c22
Pebbelzz <greeneyes14j@gmail.com>
2016-03-21 15:11:56 -0400

    Added some initial products for testing on angular

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var app = angular.module('app', []);
+
+app.controller('catTrees', function($scope){
+  $scope.catTowers = [
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Fcat%2Dresort%2Dluxury%2Dcat%2Dtree",
+      title: "Luxury Cat Tree",
+      image: "images/luxury-cat-tree.jpg",
+      description: "Cat Resort is a beautifully designed replica tree that will bring fun, exercise, relaxation and comfort to your indoor cat."
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Fsilk%2Dplants%2Ddirect%2Dmulti%2Dlevel%2Dbirch%2Dcat%2Dtree%2Dhouse%2Dand%2Dpost",
+      title: "Multi-Level Birch Cat Tree House & Post",
+      image: "images/silk-multi-level.jpg",
+      description: "Multi-Level Birch Cat Tree House and Post encourages your"+
+      " feline friends to indulge in their natural instincts while accenting " +
+      "your interior space. Birch Cat Tree House and Post is a unique creation " +
+      "that is hand-crafted in the U.S.A. and arrives fully assembled.",
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Fpurple%2Dpasison%2Dcat%2Dtree",
+      title: "Purple Passion Cat Tree",
+      image: "images/purple-passion.jpg",
+      description: "unique color and many features make it the center of " +
+      "conversation in any room. At the top of this Cat Tree is a large " +
+      "flower designed platform that is fully padded for extra comfort. " +
+      "The trunk has three holes that will surely tweak your cat's curiosity." +
+      " And the two large sculptured leaves are constructed with heat " +
+      "retention liners that provide added comfort for the sleeping or " +
+      "relaxing cat."
+    },
+    {
+      link: "http://www.shareasale.com/m-pr.cfm?merchantID=2072&userID=96241&productID=467116357",
+      title: "Sprout Lifelike Cat Tree",
+      image: "images/sprout-lifelike.jpg",
+      description: "A Pet Tree House ® is a unique hand crafted pet home." +
+      " Every home is designed and built to provide years of enjoyment for " +
+      "your dearest pets. From a small house up to the largest tree, " +
+      "each home is crafted of the highest quality. Materials are blended " +
+      "together to create a very attractive and durable home your pets " +
+      "will love."
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Flifelike%2Dmature%2Dlarge%2Dcat%2Dtree%2Dhouses",
+      image: "images/large-cat-tree-house.jpg",
+      title: "Mature Large Cat Tree Houses",
+      description: "he trees are real, and the branches are skillfully" +
+      " decorated with silk leaves. This provides a natural scratching " +
+      "post for your pet."
+    }
+  ];
+});
