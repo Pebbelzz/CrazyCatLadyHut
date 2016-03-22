6f60dbd95319956f99e4585777861346f6331284
Pebbelzz <greeneyes14j@gmail.com>
2016-03-22 15:21:51 -0400

    added more products and categories

--- js/app.js
+++ js/app.js
@@ -49,4 +49,59 @@ app.controller('catTrees', function($scope){
       "post for your pet."
     }
   ];
+
+  $scope.catShelfs = [
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2F3%2Dlevel%2Dcat%2Dwall%2Dbunker",
+      title: "Three Level Cat Wall Shelf",
+      image: "images/cat-shelf-bunker.jpg",
+      description:"This triple level cat bunker has everything a cat would " +
+      "need in any emergency situation. The top shelf is equipped with a " +
+      "plush chaise lounge and the bottom has a built in dining area. " +
+      "They can also quickly escape unexpected danger by jumping thru " +
+      "holes cut out on each level."
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Froman%2Dcat%2Dwall%2Dfort",
+      title: "Roman Cat Wall Fort",
+      image: "images/cat-wall-fort-shelf.jpg",
+      description: "The Roman Fort is the perfect full setup." +
+      "Having the hammocks attached to the bottom of the sisal poles "+
+      "allow cats to stretch and claw the poles from the bottom. "
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Fwall%2Dmounted%2Dmario%2Dcat%2Dcomplex",
+      title: "Mounted Mario Cat Complex",
+      image: "images/mario-cat-wallshelf.jpg",
+      description: "The Mario 2.0 is a hollow complex with " +
+      "sisal-lined portals to allow cats to jump into one " +
+      "and back out the other. It's a great hiding place for shy cats."
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Ftree%2Dclimber%2Dwall%2Dmounted%2Dcat%2Dtree",
+      title: "Cat Wall Mounted Cat Tree",
+      image: "images/wall-mounted-cat-tree.jpg",
+      description: "This artisan-crafted cat furniture is vertically oriented" +
+      "offering your cat lots of climbing opportunities nd chances to" +
+      "perch high above it all and observe."
+    }
+  ];
+
+  $scope.catCondos = [
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Flittlefield%2Dcuddly%2Dcat%2Dcave",
+      title: "Littlefield Cuddly Cat Cave",
+      image: "images/cat-mouse-house.jpg",
+      description: "Littlefield Cuddly Cat Cave is sure to trigger the ultimate " +
+      "game of cat and mouse while providing endless opportunities for" +
+      "cats to play, explore, scratch or just relax."
+    },
+    {
+      link: "http://www.shareasale.com/r.cfm?u=96241&b=5997&m=2072&afftrack=&urllink=www%2Ecatsplay%2Ecom%2Fso%2Ddarling%2Dcats%2Dcastle",
+      title: "Cat Castle",
+      image: "images/cat-castle.jpg",
+      description: "The So Darling Cat Castle would make an ideal addition " +
+      "to any feline kingdom."
+    }
+  ];
 });
